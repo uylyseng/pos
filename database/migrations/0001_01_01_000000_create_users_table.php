5c14fe01ee0ad4ad81f8601d18b24fc44998ff3c
lyseng <uylyseng.dev@gmail.com>
2025-02-28 08:36:05 +0700

    add migration files

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->boolean('is_active')->default(true);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('avatar')->nullable();
